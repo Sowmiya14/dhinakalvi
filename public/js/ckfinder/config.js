0657f669eb97e2954821e0031bb00a516340d46f
Sowmiya14 <sowmiyak411@gmail.com>
2018-09-24 16:39:42 +0530

    ckeditor js

---
+++ public/js/ckfinder/config.js
@@ -0,0 +1,20 @@
+/*
+ Copyright (c) 2007-2018, CKSource - Frederico Knabben. All rights reserved.
+ For licensing, see LICENSE.html or https://ckeditor.com/sales/license/ckfinder
+ */
+
+var config = {};
+
+// Set your configuration options below.
+
+// Examples:
+// config.language = 'pl';
+// config.skin = 'jquery-mobile';
+
+CKFinder.define( config );
+
+CKEDITOR.editorConfig = function( config )
+{
+    config.language = 'fr';
+    config.uiColor = '#AADC6E';
+};
